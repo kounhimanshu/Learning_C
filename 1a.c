dfe4238ae4c4808af7673855ce4b751ee4735813
Himanshu Tiwari <93237272+kounhimanshu@users.noreply.github.com>
2022-04-14 01:07:42 +0530

    Create 1a.c

---
+++ 1a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter First number: ");
+    scanf("%d", &a);
+    printf("Enter second Number: ");
+    scanf("%d", &b);
+
+    printf("Perimeter of rectangle is %d", (2*(a+b)));
+    return 0;
+}
