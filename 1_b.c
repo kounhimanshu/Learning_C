55e5b0f2d1c18985b1709d4d944692f7cb8b5e24
Himanshu Tiwari <93237272+kounhimanshu@users.noreply.github.com>
2022-04-14 01:41:33 +0530

    Create 1_b.c

---
+++ 1_b.c
@@ -0,0 +1,12 @@
+/// b. Take a number(n) from user & output its cube(n*n*n).
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    
+    printf("Cube is %d",(n*n*n));
+    return 0;
+}
